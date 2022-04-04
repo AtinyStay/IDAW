e39bdce318469f9da5baf6a391edf709bf1205b2
Hugo <hugolim2000@gmail.com>
2022-04-04 07:43:07 +0200

    caca

---
+++ Test_Graphiques/profile.php
@@ -0,0 +1,34 @@
+<body>
+    <section class="showcase">
+        <div class="container grid">
+            <div class="card">
+                <h2>Profil de l'utilisateur</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Prénom</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Sexe</th>
+                            <th scope="col">Âge</th>
+                            <th scope="col">Niveau de pratique sportive</th>
+                            <th scope="col">Poids</th>
+                            <th scope="col">Taille</th>
+                        </tr>
+                    </thead>
+                    <tbody id="profileTableBody">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+    <script>
+        $.get('http://localhost/IDAW/IMM/backend/user_profile.php',function(data) {
+            console.debug(data);
+                data = JSON.parse(data);
+                data.forEach(row => {
+                    $("#profileTableBody").append(`<tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[2]}</td><td>${row[3]}</td><td>${row[4]} ans</td><td>${row[5]}</td><td>${row[6]} kg</td><td>${row[7]} cm</td></tr>`)
+                });
+        })
+    </script>
+</body>
